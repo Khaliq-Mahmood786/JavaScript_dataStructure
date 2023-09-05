5ae77c0f2e667fb40f3f29163484d516316132bb
Khaliq-Mahmood786 <khaliq.mehmood@arhamsoft.org>
2023-09-05 20:36:22 +0500

    remove files

--- javascript.js
+++ javascript.js
@@ -1,9 +1,83 @@
-//array methods of javascript
-const arr=[1.2,2,5,"abcd",4]
-arr.push(7)   //use to insert element at end  
-arr.unshift(4); //use to insert element at the end 
-arr.shift()
-for(const item of arr)
-{
-    console.log(item)
-}
+var cron = require("node-cron");
+const Website = require("../models/website.model");
+const {
+  DnsVerification,
+  getWebsiteHealth,
+} = require("../../utils/websiteCurl");
+const EmailTemplate = require("../models/emailTemplate.model");
+const sendMail = require("../../utils/sendGridMail");
+
+exports.websiteDataSyncCron = cron.schedule("*/12788 * * * *", async () => {
+  try {
+    const websites = await Website.find();
+    const uniqueDomains = [
+      ...new Set(websites.map((website) => website.domain)),
+    ];
+
+    for (const domain of uniqueDomains) {
+      await updateWebsiteData(domain);
+    }
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+});
+
+exports.emailSchedulerForWebsiteStatus = cron.schedule(
+  "*/1477 * * * *",
+  async () => {
+    try {
+      const websitesWithBadHealth = await Website.find({ health: "Good" });
+
+      for (const website of websitesWithBadHealth) {
+        if (website.sslExpired) {
+          const userEmailAddress = website.email;
+          const emailType = "SSL Expiry Warning";
+          const emailTemplate = await EmailTemplate.findOne({
+            type: emailType,
+          });
+          const emailSubject = "SSL Certificate Expiry Warning";
+          const sendMailPromises = userEmailAddress.map((email) => {
+            return sendMail(email, emailTemplate.text, emailSubject);
+          });
+
+          await Promise.all(sendMailPromises);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+const updateWebsiteData = async (domain) => {
+  try {
+    const cleanedUrl = domain.replace(/^https?:\/\//, ""); // Remove "http://" or "https://"
+
+    const dnsResult = await DnsVerification(cleanedUrl);
+
+    const websiteHealth = await getWebsiteHealth(domain);
+    const health = websiteHealth.status;
+    const sslExpired = websiteHealth.sslExpired;
+    const sslExpiration = websiteHealth.sslExpiration;
+
+    const updatedWebsite = await Website.findOneAndUpdate(
+      { domain },
+      { health, sslExpired, sslExpiration },
+      { new: true }
+    );
+
+    if (websiteHealth.sslExpired) {
+      console.log(`${domain}: SSL certificate has expired.`);
+    } else {
+      console.log(
+        `${domain}: SSL certificate is valid for ${websiteHealth.sslExpiration} days`
+      );
+    }
+
+    return updatedWebsite;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
