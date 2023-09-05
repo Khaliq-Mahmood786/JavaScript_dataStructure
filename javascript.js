f943d8a3177b946664e4b02744b70a62af752244
Khaliq-Mahmood786 <khaliq.mehmood@arhamsoft.org>
2023-09-05 20:20:37 +0500

    add file

---
+++ javascript.js
@@ -0,0 +1,9 @@
+//array methods of javascript
+const arr=[1.2,2,5,"abcd",4]
+arr.push(7)   //use to insert element at end  
+arr.unshift(4); //use to insert element at the end 
+arr.shift()
+for(const item of arr)
+{
+    console.log(item)
+}
